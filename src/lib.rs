160acab4851429ac6eef689cec02d6ef398d0061
Falk B. Schimweg <git@falk.schimweg.de>
2026-03-06 17:55:33 +0100

    add instructions

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ pub mod mzidentml;
 pub mod polars_writer;
 
 #[pymodule]
-fn mzidentml_polars(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn _mzidentml_polars(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(polars_writer::serialize_mzidentml, m)?)?;
     m.add_function(wrap_pyfunction!(polars_writer::write_mzidentml, m)?)?;
     Ok(())
