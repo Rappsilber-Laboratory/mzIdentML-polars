d04a2ef9416c3c976229a551a05c84216bd84ca7
Falk B. Schimweg <git@falk.schimweg.de>
2026-03-19 09:56:50 +0100

    add __version__

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ pub mod polars_writer;
 
 #[pymodule]
 fn _mzidentml_polars(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add("__version__", env!("CARGO_PKG_VERSION"))?;
     m.add_function(wrap_pyfunction!(polars_writer::serialize_mzidentml, m)?)?;
     m.add_function(wrap_pyfunction!(polars_writer::write_mzidentml, m)?)?;
     Ok(())
