3590ef7d55fc9ac1e9ca39ac6300a23b4f025711
Falk B. Schimweg <git@falk.schimweg.de>
2026-04-11 10:46:54 +0200

    fix cargo deps

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use pyo3::prelude::*;
 pub mod mzidentml;
 pub mod polars_writer;
 
-#[pymodule]
+#[pymodule(gil_used = false)]
 fn _mzidentml_polars(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add("__version__", env!("CARGO_PKG_VERSION"))?;
     m.add_function(wrap_pyfunction!(polars_writer::serialize_mzidentml, m)?)?;
